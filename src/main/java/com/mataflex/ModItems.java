4aa199acdf8f35544bc983b3842dac5d332fcdd4
Sebas Mata <sebasmata09@gmail.com>
2026-02-16 14:25:06 -0600

    Fixed unused import.

--- src/main/java/com/mataflex/ModItems.java
+++ src/main/java/com/mataflex/ModItems.java
@@ -9,7 +9,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
